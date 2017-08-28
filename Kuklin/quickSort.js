c07245d9f7a7a8ebcdaadc467d9e04b81da06d47
Sergey Kuklin <ksv.crowley@gmail.com>
2017-08-28 11:42:13 +0500

    fix quickSort

--- Kuklin/quickSort.js
+++ Kuklin/quickSort.js
@@ -5,17 +5,20 @@ function quickSort(arr) {
   var middle = arr[middleIndex];
   var left = [];
   var right = [];
+  var center = [];
       
   arr.forEach(function(item) {
     if (item < middle) {
       left.push(item);
-    } else if(item > middle) {
+    } else if (item > middle) {
       right.push(item);
+    } else {
+      center.push(middle);
     }
   });
   
-  return quickSort(left).concat(middle, quickSort(right));
+  return quickSort(left).concat(center, quickSort(right));
 }
 
-var arr = [998, 645, 48, 689, 213, 15, 8, 16, 59, 11, 394, 43];
+var arr = [998, 645, 48, 689, 213, 15, 15, 8, 16, 59, 11, 394, 43];
 console.log(quickSort(arr));
